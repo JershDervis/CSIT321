fa3eb6effefbfeba649308c48d8b4e658e6fc5c5
Joshua L. Davis <joshdavisuow@gmail.com>
2021-02-24 18:38:15 +1100

    modified:   README.md 	modified:   css/main.css 	modified:   index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <div class="carousel-item active">
             <img src="assets/banners/home-banner.svg" class="d-block w-100" alt="...">
             <div class="carousel-caption d-block">
-                <h1>A Unique Driving School</h1>
+                <h1 class="font-weight-bold">A Unique Driving School</h1>
                 <h5>Offering Simulation Based</h5>
                 <h5>Online Theory Test</h5>
             </div>
@@ -14,7 +14,42 @@
 </div>
 
 <div class="container">
-
+    <h1 class="text-center" color="black">Vehicle Type</h1>
+    <p class="text-center">We offer a wide range of driving theory test preparations</p>
+    <div class="block-w">
+        <ul class="list-group list-group-horizontal">
+            <li class="list-group-item">
+                <figure class="figure">
+                    <img src="assets/icons/bike.svg" class="figure-img img-fluid rounded" alt="motorbike" unselectable="on">
+                    <figcaption class="figure-caption text-center font-weight-bold">Bike</figcaption>
+                </figure>
+            </li>
+            <li class="list-group-item">
+                <figure class="figure">
+                    <img src="assets/icons/car.svg" class="figure-img img-fluid rounded" alt="car" unselectable="on">
+                    <figcaption class="figure-caption text-center font-weight-bold">Car</figcaption>
+                </figure>
+            </li> 
+            <li class="list-group-item">
+                <figure class="figure">
+                    <img src="assets/icons/bus.svg" class="figure-img img-fluid rounded" alt="bus" unselectable="on">
+                    <figcaption class="figure-caption text-center font-weight-bold">Bus</figcaption>
+                </figure>
+            </li>  
+            <li class="list-group-item">
+                <figure class="figure">
+                    <img src="assets/icons/truck.svg" class="figure-img img-fluid rounded" alt="truck" unselectable="on">
+                    <figcaption class="figure-caption text-center font-weight-bold">Truck</figcaption>
+                </figure>
+            </li>    
+            <li class="list-group-item">
+                <figure class="figure">
+                    <img src="assets/icons/fork.svg" class="figure-img img-fluid rounded" alt="fork-lift" unselectable="on">
+                    <figcaption class="figure-caption text-center font-weight-bold">Fork-lift</figcaption>
+                </figure>
+            </li>       
+         </ul>
+    </div>
 </div>
 
 
