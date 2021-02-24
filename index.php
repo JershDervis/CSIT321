b2f56a40f9a395456930b59adbc7ee254df7ac8a
Joshua L. Davis <joshdavisuow@gmail.com>
2021-02-24 17:54:14 +1100

    modified:   assets/banners/home-banner.svg 	modified:   assets/logo.svg 	modified:   css/main.css 	modified:   index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php require_once('includes/header.php'); ?>
 
-<!-- BACKGROUND IMAGE -->
-<div class="header-bar">
-    <img id="banner" src="assets/banners/home-banner.svg" class="img-fluid" unselectable="on" draggable="false"></img>
-    <div class="top-left"><b>A Unique Driving School</b>
-        Offering Simulation Based
-        Online Theory Test
-        <div class="points"><b>1</b> - Register for Free
-            <b>2</b> - Full Qualified Team
-            <b>3</b> - Pre-Test Lessons and Theory
+<div class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/banners/home-banner.svg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-block">
+                <h1>A Unique Driving School</h1>
+                <h5>Offering Simulation Based</h5>
+                <h5>Online Theory Test</h5>
+            </div>
         </div>
     </div>
 </div>
